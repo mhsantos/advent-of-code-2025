e6fa486f97cecf49a3490678ec5076a7474de1dc
Marcel Santos <marcelmhs@yahoo.com.br>
2025-12-04 10:31:48 -0700

    Day 4 - Part 2

--- day4/main.go
+++ day4/main.go
@@ -17,19 +17,40 @@ func main() {
 	}
 
 	lines := filereader.ReadInput(filename)
-	accessible := findAccessibleRolls(lines)
+	runeMatrix := convertToRuneMatrix(lines)
+	var accessible int
+	if part == argparser.Part1 {
+		accessible = findAccessibleRolls(runeMatrix, false)
+	} else {
+		stillAccessible := findAccessibleRolls(runeMatrix, true)
+		for stillAccessible != 0 {
+			accessible += stillAccessible
+			stillAccessible = findAccessibleRolls(runeMatrix, true)
+		}
+	}
 	println("Accessible rolls:", accessible)
+}
+
+// convertToRuneMatrix converts a slice of strings to a matrix of runes to allow for easier
+// replacement of individual characters.
+func convertToRuneMatrix(lines []string) [][]rune {
+	matrix := make([][]rune, len(lines))
+	for i, line := range lines {
+		matrix[i] = []rune(line)
+	}
+	return matrix
 }
 
 // findAccessibleRolls parses the input map and return the count of rolls (cells with the @ symbol)
 // that have less than 4 adjacent rolls. This includes diagonal adjacency.
-func findAccessibleRolls(lines []string) int {
+// The replace parameters indicates if it whould replace the roll with a '.' when it's accessible.
+func findAccessibleRolls(lines [][]rune, replace bool) int {
 	count := 0
 	rows := len(lines)
 	cols := len(lines[0])
 
-	for row := 0; row < rows; row++ {
-		for col := 0; col < cols; col++ {
+	for row := range rows {
+		for col := range cols {
 			if lines[row][col] != '@' {
 				continue
 			}
@@ -51,6 +72,9 @@ func findAccessibleRolls(lines []string) int {
 			}
 			if adjacentRolls < 4 {
 				count++
+				if replace {
+					lines[row][col] = '.'
+				}
 			}
 		}
 	}

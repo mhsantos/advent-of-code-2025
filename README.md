f979c4728f44620752be112eaa5d8de54242282b
Marcel Santos <marcelmhs@yahoo.com.br>
2025-12-03 23:21:04 -0700

    Day 1 - Part 2

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ These are my solutions for the [Advent of Code 2025](https://adventofcode.com/20
 
 This year I'm using Go as the programming language.
 
+First, download the input and save it in the input folder, with the name `day<a|b|example>.txt`. `a|b|example` correspond to the Part 1, Part2 and example inputs respectively.
+
+To run the command for a given day, run `go run ./day# <part1|part2|example>`
+
 [Day 1: Secret Entrance](./day1/)
 
 [Day 2: Gift Shop]()
--- day1/main.go
+++ day1/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log/slog"
 	"strconv"
+	"strings"
 
 	"github.com/mhsantos/advent-of-code-2025/internal/argparser"
 	"github.com/mhsantos/advent-of-code-2025/internal/filereader"
@@ -20,8 +21,19 @@ func main() {
 
 	lines := filereader.ReadInput(filename)
 
+	if strings.HasSuffix(filename, "a.txt") || strings.HasSuffix(filename, "example.txt") {
+		part1(lines, true)
+	} else {
+		part1(lines, false)
+	}
+
+}
+
+func part1(lines []string, atZero bool) {
 	position := 50
 	timesAt0 := 0
+	times := 0
+	crossedTimes := 0
 
 	for idx, line := range lines {
 		steps, err := strconv.Atoi(line[1:])
@@ -31,9 +43,9 @@ func main() {
 		}
 		switch line[0] {
 		case 'L':
-			position = moveDial(position, true, steps)
+			position, times = moveDial(position, true, steps)
 		case 'R':
-			position = moveDial(position, false, steps)
+			position, times = moveDial(position, false, steps)
 		default:
 			slog.Error("invalid input", slog.Int("line number", idx), slog.String("line", line), slog.Any("error", err))
 			return
@@ -42,18 +54,34 @@ func main() {
 		if position == 0 {
 			timesAt0++
 		}
+		crossedTimes += times
 	}
-	fmt.Println("Password is:", timesAt0)
+	if atZero {
+		fmt.Println("Password is:", timesAt0)
+	} else {
+		fmt.Println("Password is:", crossedTimes)
+	}
 }
 
-// move dial moves the dial to left or right and returns the new position
-func moveDial(position int, left bool, steps int) int {
-	if left {
-		position -= steps % 100
-		if position < 0 {
-			position = 100 + position
+// move dial moves the dial to left or right and returns the new position and how many times it crossed 0
+func moveDial(position int, left bool, steps int) (int, int) {
+	times := steps / 100
+	steps = steps % 100
+	for i := 0; i < steps; i++ {
+		if left {
+			position--
+		} else {
+			position++
 		}
-		return position
+		switch position {
+		case -1:
+			position = 99
+		case 100:
+			position = 0
+			times++
+		case 0:
+			times++
+		}
 	}
-	return (position + steps) % 100
+	return position, times
 }
